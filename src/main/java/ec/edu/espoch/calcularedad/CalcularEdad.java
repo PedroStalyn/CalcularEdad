ea59ab219a4aa9bd7714a35c26fed5901b5f3caf
Pedro Aguilar <pedro_stalyn@hotmail.com>
2024-10-25 16:58:22 -0500

    Agregue el apellido y el nombre

--- src/main/java/ec/edu/espoch/calcularedad/CalcularEdad.java
+++ src/main/java/ec/edu/espoch/calcularedad/CalcularEdad.java
@@ -8,7 +8,10 @@ public class CalcularEdad {
         Scanner e=new Scanner(System.in);        
         System.out.println("Ingresa la edad:");
         System.out.println("Ingresa el nombre:");
+        System.out.println("Ingresa el apellido:");
         int edad = e.nextInt();
+        String nombre = e.nextLine();
+        String apellido = e.nextLine();
         if (edad >= 0 && edad < 120) {
             //categoriza la persona
             if (edad >= 0 && edad <= 12) {
@@ -16,7 +19,7 @@ public class CalcularEdad {
             } else if (edad >= 13 && edad <= 35) {
                 System.out.println(" Eres joven.");
             } else {
-                System.out.println(" Eres adulto.");
+                System.out.println("Hola,"+nombre+" Eres adulto.");
             }
         } else {
             System.out.println("Error!");
